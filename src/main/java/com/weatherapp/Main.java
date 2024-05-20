afbea7d44e5ebfc40a2d849bd748f634702a458b
zach <zacharymd14@gmail.com>
2024-05-20 21:15:45 +0200

    fixed project structure

---
+++ src/main/java/com/weatherapp/Main.java
@@ -0,0 +1,15 @@
+package com.weatherapp;
+
+import com.weatherapp.service.WeatherService;
+import com.weatherapp.model.WeatherData;
+
+/**
+ * Main
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+	}
+}
